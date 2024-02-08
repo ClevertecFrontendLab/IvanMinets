5f183149de06c34971e15280b0075ddd8e74d0cb
Ivan Minets2 <minetssmash@gmail.com>
2024-02-08 15:18:08 +0300

    Refactored default styles, ant design lib test

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import './main-page.css';
-import {DatePicker} from "antd";
+import {Layout} from "antd";
+import {NavMenu} from "@components/menu";
+import {useState} from "react";
+import s from "./main-page.module.css"
+import {Trigger} from "@components/trigger";
 
-export const MainPage: React.FC = () => {
+const {Header, Footer, Sider, Content} = Layout;
 
+
+export const MainPage = () => {
+    const [collapsed, setCollapsed] = useState(true);
+
+    const collapseSidebarHandler = () => setCollapsed(prevState => !prevState)
     return (
-        <>
-            <DatePicker />
-        </>
+        <Layout className={s.container}>
+            <Sider width={208}
+                   collapsedWidth={64}
+                   trigger={null}
+                   collapsible
+                   collapsed={collapsed}
+            >
+                <NavMenu isCollapsed={collapsed}/>
+                <Trigger isCollapsed={collapsed} onTrigger={collapseSidebarHandler}/>
+            </Sider>
+            <Layout>
+                <Header>Header</Header>
+                <Content>Content</Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </Layout>
     );
 };
