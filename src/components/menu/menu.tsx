717e5c6a92d0f9dae2a9b5ef6151d297f3d80671
Ivan Minets2 <minetssmash@gmail.com>
2024-02-13 11:19:53 +0300

    refactor: delete header component

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -25,7 +25,6 @@ export const NavMenu = ({isCollapsed}: Props) => {
 
             <Menu
                 className={s.menu}
-
                 mode={"vertical"}
                 items={[
                     {
